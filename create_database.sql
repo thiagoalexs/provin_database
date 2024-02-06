1816afb0e81ae0d885706b5681e0cb32d97d83d6
Thiago Álex <thiago_a@users.noreply.github.com>
2024-02-06 20:13:59 -0300

    Start configuration
    Creating database with CREATE TABLE departamentos and funcionarios.
    INSERT INTO departamentos and funcionarios.

---
+++ create_database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE provin;
+
+CREATE TABLE departamentos (
+    cod_departamento INT,
+    nome_departamento VARCHAR (30)
+);
+
+CREATE TABLE funcionarios (
+    ID varchar(5),
+    CONSTRAINT ID PRIMARY KEY (ID),
+    nome VARCHAR (50),
+    idade INT,
+    data_nascimento DATE,
+    CPF VARCHAR (11),
+    UF VARCHAR (2),
+    horario_expediente TIME,
+    data_contratacao DATE,
+    data_demissao DATE,
+    cod_departamento VARCHAR (5)
+);	
---
+++ insert_data.sql
@@ -0,0 +1,12 @@
+/*1	Administrativo
+2	Recursos Humanos
+3	Financeiro
+4	Logistica
+5	Marketing
+6	Tecnologia*/
+
+INSERT INTO departamentos (cod_departamento, nome_departamento)
+VALUE (1, "Administrativo");
+
+INSERT INTO funcionarios (ID, nome, idade, CPF, UF, cod_departamento)
+VALUE (1, "Soraia de Souza Monteiro", 36, 41205704930, "SP", 1);
